15073818080df3389506016be4321e6b79046ceb
EvanioTech <112532056+EvanioTech@users.noreply.github.com>
2024-01-27 01:49:26 -0300

    project end

--- app.js
+++ app.js
@@ -3,22 +3,25 @@ function calc() {
     const n2 = parseInt(document.getElementById('n2').value)
     if (typeOp() == 'Somar') {
         const total = n1 + n2
-        document.getElementById('result').innerHTML = 'O resultado é ' + total
+        document.getElementById('result').innerHTML = 'O resultado é ' + total + '!'
     }
     else if (typeOp() == 'Subtrair'){
         const total = n1 - n2
-        document.getElementById('result').innerHTML = 'O resultado é ' + total
+        document.getElementById('result').innerHTML = 'O resultado é ' + total + '!'
     }
     else if (typeOp() == 'Multiplicar'){
         const total = n1 * n2
-        document.getElementById('result').innerHTML = 'O resultado é ' + total
+        document.getElementById('result').innerHTML = 'O resultado é ' + total + '!'
     }
     else if (typeOp() == 'Dividir'){
         const total = n1 / n2
-        document.getElementById('result').innerHTML = 'O resultado é ' + total
+        document.getElementById('result').innerHTML = 'O resultado é ' + total + '!'
     }
     
 typeOp()
+changeColor()
+obg()
+inserirImagem()
     
 }
 function typeOp() {
@@ -27,3 +30,27 @@ function typeOp() {
   return selectedValue;
 }
 
+function changeColor() {
+    
+        
+        let newCor = document.getElementById("result");
+         newCor.style.color = "red";
+         let newsCor = document.getElementById("thanks");
+         newsCor.style.color = "DarkSlateBlue";
+}
+
+function obg() {
+   let agradecimento = document.getElementById('thanks')
+   agradecimento.innerHTML = 'Obrigador por usar nossa Calculadora!'
+}
+
+function inserirImagem() {
+
+    const minhaDiv = document.getElementById("imagem");
+
+    const imagem = document.createElement("img");
+
+    imagem.src = "./assets/a9948d44-52ca-423a-9ac0-9f05e4eee164_458x280.gif";  // Substitua pelo caminho real da sua imagem
+
+    minhaDiv.appendChild(imagem);
+}
